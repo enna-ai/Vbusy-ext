7983a2ea1f42d4d335306aef868c3c234a17a00a
enna-ai <annelee3322@gmail.com>
2023-09-19 12:32:53 -0600

    added delete and complete functionality, edit styling slightly

--- src/js/content.js
+++ src/js/content.js
@@ -7,8 +7,29 @@ const day = String(currentDate.getDate()).padStart(2, "0");
 const formatDate = `${month}/${day}`;
 todaysDate.textContent = formatDate;
 
+const token = localStorage.getItem("token");
+
+async function getUser() {
+    try {
+        const res = await fetch("http://localhost:4000/api/v1/users/profile", {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+        });
+
+        const user = await res.json();
+
+        const username = document.getElementById("username");
+        username.textContent = `@${user.username}`;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getUser();
+
 async function getTasks() {
-    const token = localStorage.getItem("token");
     if (!token) {
         console.error("Token not found in localStorage.");
         return;
@@ -27,17 +48,83 @@ async function getTasks() {
         }
 
         const data = await res.json();
-        const taskList = document.getElementById("tasks");
+        const taskList = document.getElementById("taskList");
 
         taskList.innerHTML = "";
 
         data.forEach((task) => {
             const listItem = document.createElement("li");
-            listItem.textContent = task.task;
+            listItem.setAttribute("data-task-id", task._id);
+
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.checked = task.completed;
+
+            const taskItem = document.createElement("span");
+            taskItem.textContent = task.task;
+
+            if (task.completed) {
+                listItem.classList.add("completed");
+            }
+
+            checkbox.addEventListener("change", () => {
+                const taskId = listItem.getAttribute("data-task-id");
+                completeTask(taskId);
+            });
+
+            const deleteButton = document.createElement("button");
+            deleteButton.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+            deleteButton.addEventListener("click", () => {
+                const taskId = listItem.getAttribute("data-task-id");
+                deleteTask(taskId);
+            });
+
+            listItem.appendChild(checkbox);
+            listItem.appendChild(taskItem);
+            listItem.appendChild(deleteButton);
             taskList.appendChild(listItem);
         });
     } catch (error) {
         console.error("Error fetching tasks:", error);
+    }
+}
+
+async function completeTask(taskId) {
+    try {
+        const res = await fetch(`http://localhost:4000/api/v1/tasks/${taskId}/complete`, {
+            method: "PUT",
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+        });
+
+        if (res.ok) {
+            const taskItem = document.querySelector(`li[data-task-id="${taskId}"]`);
+            taskItem.classList.toggle("completed");
+        } else {
+            console.error("Failed to toggle a task status");
+        }
+    } catch (error) {
+        console.error("An error occured trying to toggle a task as complete");
+    }
+}
+
+async function deleteTask(taskId) {
+    try {
+        const res = await fetch(`http://localhost:4000/api/v1/tasks/${taskId}`, {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+        });
+
+        if (!res.ok) {
+            throw new Error(`Failed to delete task with status code ${res.status}`);
+        }
+
+        getTasks();
+    } catch (error) {
+        console.error("An error occured trying to delete a task", error.message);
     }
 }
 
